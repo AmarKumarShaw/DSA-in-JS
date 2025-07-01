de6a8bf8a0101418f8aeefaaa5e54c4b3445289e
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-07-01 07:49:34 +0530

    Added Reverse String and MergeSortedArray

---
+++ Array-Level 0/reverseString.js
@@ -0,0 +1,9 @@
+var reverseString = function(arr) {
+    let len = arr.length;
+    let halfLen = Math.floor(len / 2);
+    for (let i = 0; i < halfLen; i++) {
+        let temp = arr[i];
+        arr[i] = arr[len - i - 1];
+        arr[len - i - 1] = temp;
+    }
+};   
