26846aec60c8ae9bc3deb94225c74f3e1f2e1ff9
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-07-02 15:26:14 +0530

    Sum of all Odd Numbers in an Array

---
+++ Array-Level 0/sumOfAllOddNumbers.js
@@ -0,0 +1,15 @@
+// Sum of All Odd Array using Recursion 
+
+function sumOdd(arr,index=0){
+    
+    if(index >= arr.length) return 0;
+    isOdd = arr[index]%2!==0;
+    if(isOdd){
+        return arr[index] + sumOdd(arr,index+1);
+    }else{
+        return sumOdd(arr,index+1);
+    }
+   
+}
+const nums =[1,2,4,4,6,5,23]
+console.log(sumOdd(nums))
