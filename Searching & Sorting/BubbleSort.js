dd18b0e8dc3ea590bb83164f03435a0c024b0403
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-07-03 14:13:36 +0530

    Optimized Sorted Array has been added

--- Searching & Sorting/BubbleSort.js
+++ Searching & Sorting/BubbleSort.js
@@ -1,20 +1,44 @@
-// Bubble Sort 
+// Bubble Sort Non-Optimized
 
 const arr = [1,4,5,6,2]
+
+// function bubble(arr){
+//     let n = arr.length;
+//     for(let i=0;i<n-1;i++){
+//         for(let j=0;j<n-1-i;j++){
+//             if(arr[j]>arr[j+1]){
+//                 let temp = arr[j];
+//                 arr[j] =  arr[j+1]
+//                 arr[j+1] = temp;
+
+//             }
+//         }
+        
+//     }
+//     return arr;
+// }
+// console.log(bubble(arr))
+
+// Optimized Bubble Sort 
 
 function bubble(arr){
     let n = arr.length;
     for(let i=0;i<n-1;i++){
-        for(let j=0;j<n-1-i;j++){
+        let swapped = false;
+        for(let j =0; j<n-1-i;j++){
             if(arr[j]>arr[j+1]){
-                let temp = arr[j];
-                arr[j] =  arr[j+1]
-                arr[j+1] = temp;
-
+                let temp =arr[j];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                swapped = true
             }
+            
         }
-        
+        // This will prevent the loop to run if there 
+        // is no sorting left
+        if(!swapped){
+            return;
+        }
+        return arr;
     }
-    return arr;
-}
-console.log(bubble(arr))
+}console.log(bubble(arr))
