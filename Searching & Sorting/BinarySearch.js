e53ba6b57a5c5328178919b72cfb415d44b25fd0
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-07-03 09:12:22 +0530

    Binary Search LeetCode 704 Code Updated

--- Searching & Sorting/BinarySearch.js
+++ Searching & Sorting/BinarySearch.js
@@ -5,18 +5,18 @@ const arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
 
 function binarySearch(arr,target){
     let left = 0;
-    let right =arr.length-1;
-    let mid = Math.floor(arr.length)/2
+    let right = arr.length-1;
     
-    while(right =>low){
+    
+    while(right>=left){
       
-        
+        let mid = Math.floor((right+left)/2)
         if(arr[mid] ==target){
             console.log("Data Found Index and Value is", mid, arr[mid])
             return mid
             
         }
-          if(target >mid){
+          if(target >arr[mid]){
             left = mid+1;
             
             
