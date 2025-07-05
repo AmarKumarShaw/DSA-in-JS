27ea9a4a2f36f7c72a02f86bd570c7782eb7e9b5
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-07-05 14:32:55 +0530

    Leet Code Problem No -> 234 Palindrom Linked List Optimal Code Solutions added

--- Linked List/PalindromeLinkedList.js
+++ Linked List/PalindromeLinkedList.js
@@ -1,4 +1,7 @@
-//  Approach 1 (Not recommended): Converting Linked List to array and then seeing that
+//  Approach 2 ( recommended)
+// Breaking the Linked List using slow and fast pointer
+// into two parts and then  comparing it accordingly
+
 var isPalindrome = function(head) {
     let arr =[];
     let curr = head;
@@ -12,4 +15,37 @@ var isPalindrome = function(head) {
         if(arr[left++]!=arr[right--])return false;
     }
     return true
+};
+
+//  Approach 1 (Not recommended): Converting Linked List to array and then seeing that
+var isPalindrome = function(head) {
+    // Finding the middle the palindrome using slow and fast pointer
+    let slow = head;
+    let fast =head
+    while(fast && fast.next){
+        slow = slow.next;
+        fast =fast.next.next
+    }
+
+    // Reversing the second half part 
+    let prev =null;
+ 
+    while(slow){
+        temp = slow.next;
+        slow.next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    // Now we need to compare first and second halves
+    let first=head;
+    let second = prev;
+
+    while(second){
+        if(first.val != second.val) return false;
+       first = first.next;
+       second = second.next;
+    }
+return true
+
 };
