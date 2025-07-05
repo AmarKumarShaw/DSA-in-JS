c4efbcb837094cf871df93a738041b7958fcd3b1
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-07-05 11:57:48 +0530

    Added Floyd's Algorithm for Linked Cycle List LeetCode Problem number -> 141

--- Linked List/LinkedListCycle.js
+++ Linked List/LinkedListCycle.js
@@ -17,3 +17,20 @@ var hasCycle = function(head) {
 
     
 };
+
+// Approach 2 :  Floyd's Cycle Algorithm
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    while(fast !=null && fast.next !=null){
+       slow = slow.next;
+      fast = fast.next.next;
+      if(slow == fast){
+          return true
+      }
+     
+    }
+  
+  return false;
+      
+  };
