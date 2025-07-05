92c2f467a443ad61f962e3827dcd6c477c10e5a9
Amar Shaw <67730823+AmarKumarShaw@users.noreply.github.com>
2025-07-05 18:44:26 +0530

    LeetCode Problem No -> 83 Remove Duplicates from Sorted List

---
+++ Linked List/RemoveDuplicateFromSortedList.js
@@ -0,0 +1,18 @@
+// Leetcode Problem #83: Remove Duplicates from Sorted List
+
+var deleteDuplicates = function(head) {
+    let senitel = new ListNode(0, head); // Create a dummy node pointing to head
+    let curr = senitel; // Start from the dummy node
+    let currSet = new Set(); // To store values we've seen
+
+    while (curr.next) { // Traverse while next node exists
+        if (currSet.has(curr.next.val)) { // If value already seen
+            curr.next = curr.next.next; // Skip the duplicate node
+        } else {
+            currSet.add(curr.next.val); // Add new value to the set
+            curr = curr.next; // Move to next node
+        }
+    }
+
+    return senitel.next; // Return the actual head (excluding dummy)
+};
